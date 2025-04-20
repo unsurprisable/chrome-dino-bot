2ae2f2bd6b742b2adda8dc000ff2da818a7c9561
Surprisable <toastedflqme@gmail.com>
2025-04-20 01:52:03 -0700

    Improved pyautogui bot
    -1500 high score
    - increased view region and improved jump timing
    - still dies to framerate  (~11fps max) due to jumping too late
    - still lacks ducking which could greatly improve longevity

--- main.py
+++ main.py
@@ -6,31 +6,60 @@ print("Open the game window! Starting 3 in seconds...")
 time.sleep(3)
 
 # region coordinates
-left = 300
-bottom = 570
+left = 233
+bottom = 542
 
 # region bounds
-width = 250
-height = 1
+width = 317
+height = 10
 
 region_bounds = (left, bottom-height, width, height)
 
-# lastTime = time.time()
+jump_cooldown = 0.20 # est. time before another jump would be needed
+time_since_jump = 0.0
+
+target_delta_time = 0.05
+delta_time = 0
+last_time = time.time()
 
 while True:
-    region = pyautogui.screenshot(region=region_bounds)
-    
-    grayscale_region = ImageOps.grayscale(region)
 
-    colors = grayscale_region.getcolors()
+    delta_time = time.time() - last_time
+    last_time = time.time()
 
-    print(f"Color data: {colors}")
+    colors: list[tuple[int, int]] = None
+
+    if time_since_jump >= jump_cooldown:
+        region = pyautogui.screenshot(region=region_bounds)
+        grayscale_region = ImageOps.grayscale(region)
+        colors = grayscale_region.getcolors()
+
+    print(f"Data: {colors}")
+    print(f"added time: {delta_time}")
+
+    time_since_jump += delta_time
 
     # if more than 1 color, theres an obstacle
-    if len(colors) > 1:
+    if colors != None and len(colors) > 1:
         pyautogui.press("space")
         print("Jump!")
+        time_since_jump = 0
+        
+    delta_time = time.time() - last_time
 
-    time.sleep(0.05) 
-    # print(f"Time Elapsed: {time.time() - lastTime}")
-    # lastTime = time.time()
+    # ensure maximum framerate
+    if delta_time < target_delta_time:
+        time.sleep(target_delta_time - delta_time)
+
+
+# while True:
+#     delta_time = time.time() - last_time
+
+#     time_since_jump += delta_time
+
+#     if time_since_jump >= jump_cooldown:
+#         pyautogui.press("space")
+#         print(f"Jump! {time_since_jump}")
+#         time_since_jump = 0
+
+#     last_time = time.time()
