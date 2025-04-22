85fe41338baac341735a84e46e3056238a5c7c47
Surprisable <toastedflqme@gmail.com>
2025-04-22 09:53:40 -0700

    Added new bot with "mss" & "pynput"
    - 2000 high score
    - much more performant (CONSISTENT 30fps)
    - seems to die to the lack of duck usage now

--- main.py
+++ main.py
@@ -1,52 +1,96 @@
-import pyautogui
 import time
-from PIL import ImageOps
+import numpy as np
+import cv2
+from mss import mss
+from pynput.keyboard import Controller
 
-print("Open the game window! Starting in 3 seconds...")
-time.sleep(3)
 
-# region coordinates
-left = 233
-bottom = 542
 
-# region bounds
-width = 317
-height = 10
+def run_bot():
+    keyboard = Controller()
+    sct = mss()
 
-region_bounds = (left, bottom-height, width, height)
+    # TODO: take screenshot of entire game area every frame and
+    # use helper methods to look through the array pixels to make
+    # the regions relative instead of all monitor-based
 
-jump_cooldown = 0.20 # est. time before another jump would be needed
-time_since_jump = 0.0
+    # screenshot region
+    obstacle_region = {
+        "top": 532,
+        "left": 233,
+        "width": 237,
+        "height": 10
+    }
 
-target_delta_time = 0.05
-delta_time = 0
-last_time = time.time()
+    # grounded_region = {
+    #     "top": 594,
+    #     "left": 169,
+    #     "width": 25,
+    #     "height": 8
+    # }
 
-while True:
+    # sky_color_region = {
+    #     "top": 700,
+    #     "left": 1350,
+    #     "width": 1,
+    #     "height": 1
+    # }
 
-    delta_time = time.time() - last_time
+    target_loop_time = 1/30
+    delta_time = 0
     last_time = time.time()
 
-    colors: list[tuple[int, int]] = None
+    # jump_cooldown = target_loop_time*3 # est. time before another jump would be needed
+    time_since_jump = 0.0 
+    jump_hold_time = 0.6 # time the bot should hold down space for max jump height
+    is_jumping = False
 
-    if time_since_jump >= jump_cooldown:
-        region = pyautogui.screenshot(region=region_bounds)
-        grayscale_region = ImageOps.grayscale(region)
-        colors = grayscale_region.getcolors()
+    print("Open the game window! Starting in 2 seconds...")
+    time.sleep(2)
 
-    print(f"Data: {colors}")
-    print(f"added time: {delta_time}")
+    keyboard.press(Controller._Key.space)
+    keyboard.release(Controller._Key.space)
 
-    time_since_jump += delta_time
+    while True:
+        current_time = time.time()
+        delta_time = time.time() - last_time
+        last_time = time.time()
+        time_since_jump += delta_time
 
-    # if there's more than 1 color, there must be an obstacle
-    if colors != None and len(colors) > 1:
-        pyautogui.press("space")
-        print("Jump!")
-        time_since_jump = 0
+        if not is_jumping:
+            frame = np.array(sct.grab(obstacle_region)) 
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            # find unique pixel colors
+            unique = np.unique(gray)
+
+            print(f"Unique shades: {len(unique)}")
+
+            # if theres more than 1 color, there must be an obstacle
+            if len(unique) > 1:
+                keyboard.press(Controller._Key.space)
+                print("Jump!")
+                time_since_jump = 0
+                is_jumping = True
+
+        elif time_since_jump >= jump_hold_time:
+            keyboard.release(Controller._Key.space)
+            is_jumping = False
         
-    delta_time = time.time() - last_time
+        # else:
+        #     frame = np.arange(sct.grab(grounded_region))
+        #     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        #     color_map = np.
+            
+        loop_time = time.time() - current_time
 
-    # ensure maximum framerate
-    if delta_time < target_delta_time:
-        time.sleep(target_delta_time - delta_time)
+        # ensure maximum framerate
+        if loop_time < target_loop_time:
+            time.sleep(target_loop_time - loop_time)
+        print(f"Time: {time.time() - current_time}")
+
+
+
+
+if __name__ == "__main__":
+    run_bot()
---
+++ pyautogui_bot.py
@@ -0,0 +1,52 @@
+import pyautogui
+import time
+from PIL import ImageOps
+
+print("Open the game window! Starting in 3 seconds...")
+time.sleep(3)
+
+# region coordinates
+left = 233
+bottom = 542
+
+# region bounds
+width = 317
+height = 10
+
+region_bounds = (left, bottom-height, width, height)
+
+jump_cooldown = 0.20 # est. time before another jump would be needed
+time_since_jump = 0.0
+
+target_delta_time = 0.05
+delta_time = 0
+last_time = time.time()
+
+while True:
+
+    delta_time = time.time() - last_time
+    last_time = time.time()
+
+    colors: list[tuple[int, int]] = None
+
+    if time_since_jump >= jump_cooldown:
+        region = pyautogui.screenshot(region=region_bounds)
+        grayscale_region = ImageOps.grayscale(region)
+        colors = grayscale_region.getcolors()
+
+    print(f"Data: {colors}")
+    print(f"added time: {delta_time}")
+
+    time_since_jump += delta_time
+
+    # if there's more than 1 color, there must be an obstacle
+    if colors != None and len(colors) > 1:
+        pyautogui.press("space")
+        print("Jump!")
+        time_since_jump = 0
+        
+    delta_time = time.time() - last_time
+
+    # ensure maximum framerate
+    if delta_time < target_delta_time:
+        time.sleep(target_delta_time - delta_time)
